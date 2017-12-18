6024f951dc0d59fd184a404464e770f719b7fe12
mlee108 <18340464+mlee108@users.noreply.github.com>
2017-12-17 23:01:27 -0600

    trying to fix cors

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ const PORT = process.env.PORT || 5000;
 
 // Allow CORS so that backend and frontend could be put on different servers
 var allowCrossDomain = function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
+	res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 };
 app.use(allowCrossDomain);
 
